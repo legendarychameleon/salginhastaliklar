1c54fba088e20d9724fbc3817b98574d5e5e14b1
legendarychameleon <136637741+legendarychameleon@users.noreply.github.com>
2025-04-05 21:08:24 +0300

    script

---
+++ script.js
@@ -0,0 +1,48 @@
+function Function1() {
+    document.getElementById("2").innerHTML = "Hava yoluyla yayılan hastalıklar arasında grip, soğuk algınlığı, su çiçeği, kızamık, kabakulak ve tüberküloz gibi hastalıklar bulunmaktadır.";
+    document.getElementById("3").innerHTML = "Bu hastalıklar genellikle virüsler veya bakteriler tarafından yayılır ve enfekte bir kişinin öksürmesi, hapşırması veya konuşması yoluyla havaya yayılan damlacıklar aracılığıyla başkalarına bulaşabilir.";
+    document.getElementById("4").innerHTML = "Bu hastalıklardan korunmak için el hijyenine dikkat etmek, kalabalık ortamlardan kaçınmak ve aşı olmak önemlidir.";
+    document.getElementById("5").innerHTML = "Ayrıca, hasta olan kişilerle temastan kaçınmak ve maske takmak da bulaşma riskini azaltabilir.";
+    document.getElementById("6").innerHTML = "Bu hastalıkların belirtileri genellikle ateş, öksürük, boğaz ağrısı ve baş ağrısı gibi semptomları içerir.";
+    document.getElementById("7").innerHTML = "Eğer bu belirtileri yaşıyorsanız, bir doktora başvurmanız önemlidir.";  
+  }
+  function Function2() {
+    document.getElementById("9").innerHTML = "Su yoluyla yayılan hastalıklar arasında kolera, tifo, hepatit A ve dizanteri gibi hastalıklar bulunmaktadır.";
+    document.getElementById("10").innerHTML = "Bu hastalıklar genellikle kirli su kaynakları veya kontamine gıdalar yoluyla bulaşır.";
+    document.getElementById("11").innerHTML = "Su yoluyla yayılan hastalıklardan korunmak için temiz su içmek, iyi hijyen uygulamak ve güvenilir gıda kaynaklarından beslenmek önemlidir.";
+    document.getElementById("12").innerHTML = "Ayrıca, suyun kaynatılması veya filtrelenmesi gibi yöntemler de su kaynaklarını güvenli hale getirebilir. Ayrıca sebze ve meyveleri iyice yıkamak da önemlidir."; 
+    document.getElementById("13").innerHTML = "Bu hastalıkların belirtileri genellikle ishal, karın ağrısı, ateş ve kusma gibi semptomları içerir.";
+    document.getElementById("14").innerHTML = "Eğer bu belirtileri yaşıyorsanız, bir doktora başvurmanız önemlidir.";
+  }
+  function Function3() {
+    document.getElementById("15").innerHTML = "Anneden bebeğe geçen hastalıklar arasında HIV, hepatit B ve C, sifiliz ve rubella gibi hastalıklar bulunmaktadır.";
+    document.getElementById("16").innerHTML = "Bu hastalıklar genellikle doğum sırasında, emzirme yoluyla veya gebelik sırasında anneden bebeğe geçebilir.";
+    document.getElementById("17").innerHTML = "Anneden bebeğe geçen hastalıklardan korunmak için gebelik öncesi sağlık kontrolleri, düzenli doktor ziyaretleri ve aşılar önemlidir.";  
+    
+  }
+  function Function4() {
+    document.getElementById("18").innerHTML = "Kan yoluyla yayılan hastalıklar arasında HIV, hepatit B ve C, sifiliz ve Lyme hastalığı gibi hastalıklar bulunmaktadır.";
+    document.getElementById("19").innerHTML = "Bu hastalıklar genellikle enfekte kan veya vücut sıvıları yoluyla bulaşır.";
+    document.getElementById("20").innerHTML = "Kan yoluyla yayılan hastalıklardan korunmak için güvenli cinsel ilişki, kan transfüzyonları sırasında dikkatli olmak önemlidir.";
+    document.getElementById("21").innerHTML = "Ayrıca, şırıngalarda ve serumlarda kullanılan iğneler ile vücuda giren tıbbi araçların tekrar kullanılmamak üzere imha edilmiş olması ve daha önce kullanılmamışlar ise dezenfekte edilmiş olduklarından emin olmak önemlidir. Kan yoluyla yayılan hastalıkların çoğu bu araçların kullanımındaki ihmalsizliklerden kaynaklanır";
+
+  }
+  function Function5() {
+    document.getElementById("viral").innerHTML = "Viral enfeksiyonlar, virüslerin vücut içine girmesiyle oluşan enfeksiyon türüdür ve bulaşıcı hastalıkların kaynaklarından biridir. Bu tür enfeksiyonların başında soğuk algınlığı ve kızamık gelir.";
+  }
+  function Function6() {
+    document.getElementById("bakteriyel").innerHTML = "Bakteriler, küçük bir DNA parçasına yazılmış tek hücreli organizmalar olarak kabul edilir. Bakteriler, vücudun içi ve cilt dahil olmak üzere vücudun her yerinde bulunabilir. Birçok bakteri zararsız veya hatta faydalıdır ancak bazı bakteriler bulaşıcı hastalığa neden olabilecek toksinler salgılayabilir.";
+  }
+  function Function7() {
+    document.getElementById("mantar").innerHTML = "Bakteriler gibi vücutta birçok farklı mantar türü bulunur. Bunlar vücudun farklı bölümlerinde yaşayabilir. Mantarlar aşırı büyüdüğünde veya zararlı mantarlar ağız, burun veya ciltteki bir kesikten vücuda girdiğinde hastalığa sebebiyet verebilir.";
+  }
+  function Function8() {
+    document.getElementById("parazit").innerHTML = "Parazitler yaşamak, üremek ve canlı kalmak için diğer organizmaların vücutlarını kullanırlar. Parazitler arasında solucanlar (helmintler) ve bazı tek hücreli organizmalar (protozoa) yer alır.";
+  }
+  function Function9() {
+    document.getElementById("virus").innerHTML = "Virüs, canlı hücreleri enfekte edebilen ve böylece replike olabilen mikroskobik enfeksiyon etkenleridir. Virüsler; hayvanlardan ve bitkilerden, bakterilerin ve arkelerin de içinde bulunduğu mikroorganizmalara kadar her türlü canlı şekillerine bulaşabilirler.";
+  }
+  function Function10() {
+    document.getElementById("bakteri").innerHTML = "Bakteriler tek hücreli mikroorganizma grubudur. Tipik olarak birkaç mikrometre uzunluğunda olan bakterilerin çeşitli şekilleri vardır, kimi küresel, kimi spiral şekilli, kimi çubuksu, kimi virgül şeklinde olabilir. Yeryüzündeki hemen hemen her ortamda bakteriler mevcuttur. Toprakta, deniz suyunda, okyanusun derinliklerinde, yer kabuğunda, deride, hayvanların bağırsaklarında, asitli sıcak su kaynaklarında, radyoaktif atıklarda büyüyebilen tipleri vardır. Tipik olarak bir gram toprakta bulunan bakteri hücrelerinin sayısı 40 milyon, bir mililitre tatlı suda ise bir milyondur; toplu olarak dünyada beş nonilyon (5×1030) bakteri bulunmaktadır,bunlar dünyadaki biyokütlenin çoğunu oluşturur. Bakteriler gıdaların geri dönüşümü için hayati bir öneme sahiptirler ve gıda döngülerindeki çoğu önemli adım, atmosferden azot fiksasyonu gibi, bakterilere bağlıdır. Ancak bu bakterilerin çoğu henüz tanımlanmamıştır ve bakteri şubelerinin sadece yaklaşık yarısı laboratuvarda kültürlenebilen türlere sahiptir. Bakterilerin araştırıldığı bilim bakteriyolojidir. Bu, mikrobiyolojinin bir dalıdır. ";
+  }
+
